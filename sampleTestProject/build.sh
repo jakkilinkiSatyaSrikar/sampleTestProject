3dca72dad5ccc55beb00dd6b9dd2b846b4c7fc75
jakkilinkiSatyaSrikar <satyasrikarjakkilinki@gmail.com>
2026-02-09 19:41:53 +0530

    Add Django to requirements

--- sampleTestProject/build.sh
+++ sampleTestProject/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
+
+# Install libraries
 pip install -r requirements.txt
+
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
